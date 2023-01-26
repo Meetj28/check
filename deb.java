a799858994a9d2dbd3f3f3ca39f4244ab1373477
Meetj28 <meetj.jain576@gmail.com>
2023-01-26 23:49:04 +0530

    check git

---
+++ deb.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class deb
+{
+public static void main(String args[])
+{
+int a;
+System.out.println("hello");
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+System.out.println(a);
+}
+}
